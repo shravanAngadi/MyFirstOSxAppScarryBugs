889a29efb16b86350da8885bb21bf3dbc98e1dff
Airatch <airatch@Airatchs-MacBook-Pro.local>
2015-09-26 12:15:20 +0530

    Initial Commit

---
+++ ScarryBugsCocoa/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScarryBugsCocoa
+//
+//  Created by Airatch on 9/26/15.
+//  Copyright (c) 2015 AirWatch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ScarryBugsCocoa/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ScarryBugsCocoa
+//
+//  Created by Airatch on 9/26/15.
+//  Copyright (c) 2015 AirWatch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
